9820eac6ab9734e5b3ae3d55b7c28a5a834370e3
morgannito <mriu.morgannito@gmail.com>
2020-11-26 16:20:55 +0100

    ajout de fonts

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ fenetre.geometry("1920x1080")
 fenetre.minsize(1080,720)
 #logo de la fenetre
 fenetre.iconbitmap("ressources/images/logo/slender-remix.ico")
-
+#couleur de fond de la fenetre
+fenetre.config(background='#1ac0ff')
+#ajout du titre du jeu dans la fenetre
+title = Label(fenetre, text="Slender Remix 2D ", font=("caveat",40),bg="#1ac0ff",fg="white")
+title.pack()
 
 #affiche la fenetre
 fenetre.mainloop()
