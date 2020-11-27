9260c3456930cb91502b369a96091d83d6313a34
morgannito <mriu.morgannito@gmail.com>
2020-11-27 02:42:31 +0100

    button centrer sur l'image

--- main.py
+++ main.py
@@ -17,25 +17,26 @@ fenetre.config(background="black")
 title = Label(fenetre, text="Slender Remix 2D ", font=("caveat",40),bg="black",fg="white")
 #ajoute le titre
 title.pack()
-fenetre.image = PhotoImage(file='ressources/images/menu/fantasy-2847724_1920.png')
-fenetre.w, fenetre.h = fenetre.image.width(), fenetre.image.height()
-fenetre.canvas = Canvas(fenetre, width=fenetre.w, height=fenetre.h,bd=0 ,highlightthickness=0)
-fenetre.canvas.pack(expand=YES)
-fenetre.canvas.create_image((fenetre.w // 2, fenetre.h // 2), image=fenetre.image)
 
 #creation d'une frame
 frame = Frame(fenetre, bg="#1ac0ff")
+#ajouter la frame
+frame.pack(expand=YES)
 
-
+fenetre.image = PhotoImage(file='ressources/images/menu/fantasy-2847724_1920.png')
+fenetre.w, fenetre.h = fenetre.image.width(), fenetre.image.height()
+fenetre.canvas = Canvas(frame, width=fenetre.w, height=fenetre.h,bd=0 ,highlightthickness=0)
+fenetre.canvas.pack()
+fenetre.canvas.create_image((fenetre.w // 2, fenetre.h // 2), image=fenetre.image)
 
 
 #ajout d'un bouton dans la frame
-button_play = Button(fenetre, text="play",font=("caveat",40),bg="black",fg="white")
-button_play.pack()
+button_play = Button(frame, text="play",font=("caveat",40),bg="black",fg="white")
+button_play = fenetre.canvas.create_window(fenetre.w // 2,fenetre.h // 2,anchor='center',window=button_play)
 
 
-#ajouter la frame
-frame.pack()
+
+
 
 #affiche la fenetre
 fenetre.mainloop()
