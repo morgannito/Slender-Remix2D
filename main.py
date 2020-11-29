061b2503030dc74c73e7035f61a7667255a9c8cc
morgannito <mriu.morgannito@gmail.com>
2020-11-29 19:55:06 +0100

    hash mdp

--- main.py
+++ main.py
@@ -107,10 +107,56 @@ def login():
             x = requests.post(url, data=jsondata)
             x = json.loads(x.text)
             if (x['erreur'] == 1) :
-                print("erreur mdp ou login")
+                # Rafraichi la fentre pour afficher la confirmation de l'enregistrement
+                for widget in fenetre.winfo_children():
+                    widget.pack_forget()
+                title_play = Label(fenetre, text="Slender Remix 2D", font=("caveat", 40), bg="black", fg="white")
+                title_play.pack()
+                frame = Frame(fenetre, bg="black")
+                # Ajouter la frame
+                frame.pack(expand=YES)
+                # Création de l'image de fond pour la fenetre
+                fenetre.image = PhotoImage(file='ressources/images/menu/fantasy-2847724_1920.png')
+                fenetre.w, fenetre.h = fenetre.image.width(), fenetre.image.height()
+                fenetre.canvas = Canvas(frame, width=fenetre.w, height=fenetre.h, bd=0, highlightthickness=0)
+                fenetre.canvas.pack()
+                fenetre.canvas.create_image((fenetre.w // 2, fenetre.h // 2), image=fenetre.image)
+                # Ajout du champ login
+                Confirm = Label(frame, font=("caveat", 40), bg="black", fg="white", text="Erreur Mot de passe invalide ou login invalide")
+                Confirm = fenetre.canvas.create_window(fenetre.w // 2, fenetre.h // 2,
+                                                       anchor='center', window=Confirm)
+                # Rafraichie
+                frame.update()
+                # Une ptite pause pour laisser le message s'afficher !
+                time.sleep(5)
+                # Renvoie sur la page de login  cause erreur mdp!
+                play()
             else :
-                print("go play")
-
+                for widget in fenetre.winfo_children():
+                    widget.pack_forget()
+                title_play = Label(fenetre, text="Slender Remix 2D", font=("caveat", 40), bg="black", fg="white")
+                title_play.pack()
+                frame = Frame(fenetre, bg="black")
+                # Ajouter la frame
+                frame.pack(expand=YES)
+                # Création de l'image de fond pour la fenetre
+                fenetre.image = PhotoImage(file='ressources/images/menu/fantasy-2847724_1920.png')
+                fenetre.w, fenetre.h = fenetre.image.width(), fenetre.image.height()
+                fenetre.canvas = Canvas(frame, width=fenetre.w, height=fenetre.h, bd=0, highlightthickness=0)
+                fenetre.canvas.pack()
+                fenetre.canvas.create_image((fenetre.w // 2, fenetre.h // 2), image=fenetre.image)
+                # Ajout du champ login
+                Confirm = Label(frame, font=("caveat", 40), bg="black", fg="white",
+                                text="Connexion en cours ...")
+                Confirm = fenetre.canvas.create_window(fenetre.w // 2, fenetre.h // 2,
+                                                       anchor='center', window=Confirm)
+                # Rafraichie
+                frame.update()
+                # Une ptite pause pour laisser le message s'afficher !
+                time.sleep(5)
+                # Renvoie sur la page de login  cause erreur mdp!
+                play()
+                print("Lance le jeu ")
 
 # Fonction button inscription
 # Page Inscription au jeu
