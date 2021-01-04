bcea0fcf19084c150d146fbccadfec66237c3298
morga <mriu.morgannito@gmail.com>
2021-01-04 17:57:52 +0100

    up

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import module.menu as menu
-
+import os
+import sys
+from art import *
+from termcolor import colored
 # Lance le programme :) !
+
+print(colored(text2art("Slender"), 'cyan'))
+print(colored('Created by Morgannito \n\n'.center(120), 'red'))
+if sys.version_info.major < 3:
+    print("python version may be > major 3")
+    exit(0)
+
 menu.main()
